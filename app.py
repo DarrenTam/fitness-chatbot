f13812de69ac0abc864771137ab454facdcc8367
Darren Tam <darrentamhk@gmail.com>
2021-04-07 01:17:47 +0800

    updated circle ci config.yml

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ from service.chat_bot import start_chatbot
 
 chat_bot_controller = False
 app = Flask(__name__)
-sched = BackgroundScheduler(daemon=True)
-
-
-def start_chatbot():
-    global chat_bot_controller
-    chat_bot_controller = True
-    start_chatbot()
-
-
-sched.add_job(start_chatbot, 'interval', seconds=1, id="123")
-sched.start()
-
+# sched = BackgroundScheduler(daemon=True)
+#
+#
+# def start_chatbot():
+#     global chat_bot_controller
+#     chat_bot_controller = True
+#     start_chatbot()
+#
+#
+# sched.add_job(start_chatbot, 'interval', seconds=1, id="123")
+# sched.start()
+#
 
 @app.route('/')
 def health_check():
-    if chat_bot_controller:
-        sched.remove_all_jobs()
+    # if chat_bot_controller:
+    #     sched.remove_all_jobs()
     return 'up'
 
 if __name__ == '__main__':
